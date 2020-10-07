6668f927a54d28c4674e81a04e63402e9326df37
justgreggy <greg.georg.popov@gmail.com>
2020-10-07 11:49:17 +0300

    Min clean-up, dep updates

--- vero.php
+++ vero.php
@@ -1,85 +1,154 @@
 <?php
 
-  if (!function_exists('json_encode')) {
+if (!function_exists('json_encode')) {
     throw new Exception('Vero needs the JSON PHP extension.');
-  }
+}
 
-  require(dirname(__FILE__) . '/vero/client.php');
+require(dirname(__FILE__) . '/vero/client.php');
 
-  class Vero {
-
+/**
+ * Class Vero
+ */
+class Vero
+{
+    /**
+     * @var Vero\Client
+     */
     private $client;
 
-    public function __construct($auth_token) {
-      if (!$auth_token)
-        throw new Exception("VeroClient auth_token parameter is required");
+    /**
+     * Vero constructor.
+     * @param  $auth_token
+     *
+     * @throws Exception
+     */
+    public function __construct($auth_token)
+    {
+        if (!$auth_token)
+            throw new Exception("VeroClient auth_token parameter is required");
 
-      $this->client = new Vero\Client($auth_token);
+        $this->client = new Vero\Client($auth_token);
     }
 
-    public function identify($user_id, $email = null, $data = array()) {
-      if (!$user_id)
-        throw new Exception("Vero::Identify requires a user id");
+    /**
+     * @param  $user_id
+     * @param  null $email
+     * @param  array $data
+     *
+     * @return bool|string
+     *
+     * @throws Exception
+     */
+    public function identify($user_id, $email = null, $data = [])
+    {
+        if (!$user_id)
+            throw new Exception("Vero::Identify requires a user id");
 
-      return $this->client->identify($user_id, $email, $data);
+        return $this->client->identify($user_id, $email, $data);
     }
 
-    public function reidentify($user_id, $new_user_id) {
-      if (!$user_id || !$new_user_id)
-        throw new Exception("Vero::Reidentify requires a user id AND a new user id");
+    /**
+     * @param $user_id
+     * @param $new_user_id
+     * @return bool|string
+     * @throws Exception
+     */
+    public function reidentify($user_id, $new_user_id)
+    {
+        if (!$user_id || !$new_user_id)
+            throw new Exception("Vero::Reidentify requires a user id AND a new user id");
 
-      return $this->client->reidentify($user_id, $new_user_id);
+        return $this->client->reidentify($user_id, $new_user_id);
     }
 
-    public function update($user_id, $changes = array()) {
-      if (!$user_id)
-        throw new Exception("Vero::Update requires a user id");
+    /**
+     * @param $user_id
+     * @param array $changes
+     * @return bool|string
+     * @throws Exception
+     */
+    public function update($user_id, $changes = [])
+    {
+        if (!$user_id)
+            throw new Exception("Vero::Update requires a user id");
 
-      if ($changes == array())
-        throw new Exception("Vero::Update requires changes param");
+        if ($changes == [])
+            throw new Exception("Vero::Update requires changes param");
 
-      return $this->client->update($user_id, $changes);
+        return $this->client->update($user_id, $changes);
     }
 
-    public function tags($user_id, $add = array(), $remove = array()) {
-      if (!$user_id)
-        throw new Exception("Vero::Tags requires a user id");
+    /**
+     * @param $user_id
+     * @param array $add
+     * @param array $remove
+     * @return bool|string
+     * @throws Exception
+     */
+    public function tags($user_id, $add = [], $remove = [])
+    {
+        if (!$user_id)
+            throw new Exception("Vero::Tags requires a user id");
 
-      if ($add == array() && $remove == array())
-        throw new Exception("Vero::Update requires either add or remove param");
+        if ($add == [] && $remove == [])
+            throw new Exception("Vero::Update requires either add or remove param");
 
-      return $this->client->tags($user_id, $add, $remove);
+        return $this->client->tags($user_id, $add, $remove);
     }
 
-    public function unsubscribe($user_id, $email) {
-      if ($user_id && $email)
-        throw new Exception("Vero::Unsubscribe requires either user id or email param");
+    /**
+     * @param $user_id
+     * @param $email
+     * @return bool|string
+     * @throws Exception
+     */
+    public function unsubscribe($user_id, $email)
+    {
+        if ($user_id && $email)
+            throw new Exception("Vero::Unsubscribe requires either user id or email param");
 
-      if (!$user_id && !$email)
-        throw new Exception("Vero::Unsubscribe requires a user id or email");
+        if (!$user_id && !$email)
+            throw new Exception("Vero::Unsubscribe requires a user id or email");
 
-      return $this->client->unsubscribe($user_id, $email);
+        return $this->client->unsubscribe($user_id, $email);
     }
 
-    public function resubscribe($user_id, $email) {
-      if ($user_id && $email)
-        throw new Exception("Vero::Resubscribe requires either user id or email param");
+    /**
+     * @param $user_id
+     * @param $email
+     * @return bool|string
+     * @throws Exception
+     */
+    public function resubscribe($user_id, $email)
+    {
+        if ($user_id && $email)
+            throw new Exception("Vero::Resubscribe requires either user id or email param");
 
-      if (!$user_id && !$email)
-        throw new Exception("Vero::Resubscribe requires a user id or email");
+        if (!$user_id && !$email)
+            throw new Exception("Vero::Resubscribe requires a user id or email");
 
-      return $this->client->resubscribe($user_id, $email);
+        return $this->client->resubscribe($user_id, $email);
     }
 
-    public function track($event_name, $identity = array(), $data = array(), $extras = array()) {
-      if (!$event_name)
-        throw new Exception("Vero::Track requires an event name");
-      if (($identity == array()) || ((gettype($identity) == 'array') && (!$identity['id'])))
-        throw new Exception("Vero::Update requires an identity profile with at least an id property");
+    /**
+     * @param $event_name
+     * @param array $identity
+     * @param array $data
+     * @param array $extras
+     * @return bool|string
+     * @throws Exception
+     */
+    public function track($event_name, $identity = [], $data = [], $extras = [])
+    {
+        if (!$event_name)
+            throw new Exception("Vero::Track requires an event name");
 
-      return $this->client->track($event_name, $identity, $data, $extras);
+        if (($identity == []) || ((gettype($identity) == 'array') && (!$identity['id'])))
+            throw new Exception("Vero::Update requires an identity profile with at least an id property");
+
+        return $this->client->track($event_name, $identity, $data, $extras);
     }
 
-  }
+}
 
 ?>
--- vero/client.php
+++ vero/client.php
@@ -1,123 +1,195 @@
 <?php
 
-  namespace Vero;
+namespace Vero;
 
-  class Client {
+/**
+ * Class Client
+ * @package Vero
+ */
+class Client
+{
 
+    /**
+     * @var
+     */
     private $auth_token;
 
-    public function __construct($auth_token) {
-      $this->auth_token = $auth_token;
+    /**
+     * Client constructor.
+     *
+     * @param $auth_token
+     */
+    public function __construct($auth_token)
+    {
+        $this->auth_token = $auth_token;
     }
 
-    public function identify($user_id, $email, $data) {
-      $endpoint = "https://api.getvero.com/api/v2/users/track.json";
-      $request_data = array(
-        'auth_token'        => $this->auth_token,
-        'id'                => $user_id,
-        'data'              => ($data == array() ? NULL : $data)
-      );
+    /**
+     * @param  $user_id
+     * @param  $email
+     * @param  $data
+     *
+     * @return bool|string
+     */
+    public function identify($user_id, $email, $data)
+    {
+        $endpoint = "https://api.getvero.com/api/v2/users/track.json";
+        $request_data = [
+            'auth_token' => $this->auth_token,
+            'id'         => $user_id,
+            'data'       => ($data == [] ? NULL : $data)
+        ];
 
-      if ($email)
-        $request_data['email'] = $email;
+        if ($email)
+            $request_data['email'] = $email;
 
-      return $this->_send($endpoint, $request_data);
+        return $this->_send($endpoint, $request_data);
     }
 
-    public function reidentify($user_id, $new_user_id) {
-      $endpoint = "https://api.getvero.com/api/v2/users/reidentify.json";
-      $request_data = array(
-        'auth_token'        => $this->auth_token,
-        'id'                => $user_id,
-        'new_id'            => $new_user_id
-      );
+    /**
+     * @param  $user_id
+     * @param  $new_user_id
+     *
+     * @return bool|string
+     */
+    public function reidentify($user_id, $new_user_id)
+    {
+        $endpoint = "https://api.getvero.com/api/v2/users/reidentify.json";
+        $request_data = array(
+            'auth_token' => $this->auth_token,
+            'id'         => $user_id,
+            'new_id'     => $new_user_id
+        );
 
-      return $this->_send($endpoint, $request_data, 'put');
+        return $this->_send($endpoint, $request_data, 'put');
     }
 
-    public function update($user_id, $changes) {
-      $endpoint = "https://api.getvero.com/api/v2/users/edit.json";
-      $request_data = array(
-        'auth_token'        => $this->auth_token,
-        'id'                => $user_id,
-        'changes'           => $changes
-      );
+    /**
+     * @param  $user_id
+     * @param  $changes
+     *
+     * @return bool|string
+     */
+    public function update($user_id, $changes)
+    {
+        $endpoint = "https://api.getvero.com/api/v2/users/edit.json";
+        $request_data = [
+            'auth_token' => $this->auth_token,
+            'id'         => $user_id,
+            'changes'    => $changes
+        ];
 
-      return $this->_send($endpoint, $request_data, 'put');
+        return $this->_send($endpoint, $request_data, 'put');
     }
 
-    public function tags($user_id, $add, $remove) {
-      $endpoint = "https://api.getvero.com/api/v2/users/tags/edit.json";
-      $request_data = array(
-        'auth_token'        => $this->auth_token,
-        'id'                => $user_id,
-        'add'               => $add,
-        'remove'            => $remove
-      );
+    /**
+     * @param  $user_id
+     * @param  $add
+     * @param  $remove
+     *
+     * @return bool|string
+     */
+    public function tags($user_id, $add, $remove)
+    {
+        $endpoint = "https://api.getvero.com/api/v2/users/tags/edit.json";
+        $request_data = [
+            'auth_token' => $this->auth_token,
+            'id'         => $user_id,
+            'add'        => $add,
+            'remove'     => $remove
+        ];
 
-      return $this->_send($endpoint, $request_data, 'put');
+        return $this->_send($endpoint, $request_data, 'put');
     }
 
-    public function unsubscribe($user_id, $email) {
-      $endpoint = "https://api.getvero.com/api/v2/users/unsubscribe.json";
-      $request_data = array(
-        'auth_token'        => $this->auth_token,
-        'id'                => $user_id,
-        'email'             => $email
-      );
+    /**
+     * @param  $user_id
+     * @param  $email
+     *
+     * @return bool|string
+     */
+    public function unsubscribe($user_id, $email)
+    {
+        $endpoint = "https://api.getvero.com/api/v2/users/unsubscribe.json";
+        $request_data = [
+            'auth_token' => $this->auth_token,
+            'id' => $user_id,
+            'email' => $email
+        ];
 
-      return $this->_send($endpoint, $request_data);
+        return $this->_send($endpoint, $request_data);
     }
 
-    public function resubscribe($user_id, $email) {
-      $endpoint = "https://api.getvero.com/api/v2/users/resubscribe.json";
-      $request_data = array(
-        'auth_token'        => $this->auth_token,
-        'id'                => $user_id,
-        'email'             => $email
-      );
+    /**
+     * @param  $user_id
+     * @param  $email
+     *
+     * @return bool|string
+     */
+    public function resubscribe($user_id, $email)
+    {
+        $endpoint = "https://api.getvero.com/api/v2/users/resubscribe.json";
+        $request_data = [
+            'auth_token' => $this->auth_token,
+            'id'         => $user_id,
+            'email'      => $email
+        ];
 
-      return $this->_send($endpoint, $request_data);
+        return $this->_send($endpoint, $request_data);
     }
 
-    public function track($event_name, $identity, $data, $extras = array()) {
-      $endpoint = "https://api.getvero.com/api/v2/events/track.json";
-      $request_data = array(
-        'auth_token'        => $this->auth_token,
-        'identity'          => $identity,
-        'event_name'        => $event_name,
-        'data'              => ($data == array() ? NULL : $data),
-        'extras'            => ($extras == array() ? NULL : $extras)
-      );
+    /**
+     * @param  $event_name
+     * @param  $identity
+     * @param  $data
+     * @param  array $extras
+     *
+     * @return bool|string
+     */
+    public function track($event_name, $identity, $data, $extras = [])
+    {
+        $endpoint = "https://api.getvero.com/api/v2/events/track.json";
+        $request_data = [
+            'auth_token' => $this->auth_token,
+            'identity' => $identity,
+            'event_name' => $event_name,
+            'data' => ($data == [] ? NULL : $data),
+            'extras' => ($extras == [] ? NULL : $extras)
+        ];
 
-      return $this->_send($endpoint, $request_data);
+        return $this->_send($endpoint, $request_data);
     }
 
-    private function _send($endpoint, $request_data, $request_type = 'post') {
-      $request_data = json_encode($request_data);
+    /**
+     * @param  string $endpoint
+     * @param  array  $request_data
+     * @param  string $request_type
+     *
+     * @return bool|string
+     */
+    private function _send($endpoint, $request_data, $request_type = 'post')
+    {
+        $request_data = json_encode($request_data);
+        $headers = ['Accept: application/json', 'Content-Type: application/json'];
+        $handle = curl_init();
 
-      $headers  = array('Accept: application/json', 'Content-Type: application/json');
+        curl_setopt($handle, CURLOPT_URL, $endpoint);
+        curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+        $request_type === 'post' ?
+            curl_setopt($handle, CURLOPT_POST, true) :
+            curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($handle, CURLOPT_POSTFIELDS, $request_data);
 
-      $handle = curl_init();
-      curl_setopt($handle, CURLOPT_URL, $endpoint);
-      curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
-      curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-      if ($request_type == 'post') {
-        curl_setopt($handle, CURLOPT_POST, true);
-      } elseif ($request_type == 'put') {
-        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "PUT");
-      }
-      curl_setopt($handle, CURLOPT_POSTFIELDS, $request_data);
+        $result = curl_exec($handle);
+        $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+        curl_close($handle);
 
-      $result = curl_exec($handle);
-      $code   = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-      curl_close($handle);
-
-      return $result;
+        return $result;
     }
 
-  }
+}
 
 ?>
